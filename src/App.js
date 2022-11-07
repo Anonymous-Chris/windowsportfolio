daedab32be07633273244cd791d4d10fa613d026
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-11-06 23:48:33 -0600

    add  a background and a taskbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import TaskBar from './components/TaskBar/TaskBar';
 function App() {
   return (
     <div className="App">
       portfolio 
+      <div className='app_taskbar'>
+        <TaskBar></TaskBar>
+      </div>
     </div>
   );
 }
---
+++ src/components/TaskBar/TaskBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../assets/styles/taskbar.css'
+const TaskBar = () => {
+  return (
+    <div className='taskbar'>TaskBar</div>
+  )
+}
+
+export default TaskBar
