f5a2ff91fd5258edf1f274d19f86e56deb5505f0
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-11-07 23:10:15 -0600

    styled using prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import TaskBar from './components/TaskBar/TaskBar';
+import "./App.css";
+import TaskBar from "./components/TaskBar/TaskBar";
 function App() {
-  return (
-    <div className="App">
-      portfolio 
-      <div className='app_taskbar'>
-        <TaskBar></TaskBar>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            portfolio
+            <div className="app_taskbar">
+                <TaskBar></TaskBar>
+            </div>
+        </div>
+    );
 }
 
 export default App;
