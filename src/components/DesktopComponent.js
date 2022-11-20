bdbb3ef6e41b1b90c1e1b248952663649ea3e1af
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-11-20 01:54:45 -0600

    add change theme function

--- src/components/DesktopComponent.js
+++ src/components/DesktopComponent.js
@@ -1,15 +1,28 @@
 import React from "react"
+import { useStateValue } from "../context-api/StateProvider"
 import DesktopIconsPanel from "./DesktopIconsPanel/DesktopIconsPanel"
 import TaskBar from "./TaskBar/TaskBar"
 const DesktopComponent = () => {
+    const [{ darkTheme }, dispatch] = useStateValue()
+    const changeTheme = () => {
+        dispatch({
+            type: "UPDATE_THEME",
+            darkTheme: !darkTheme,
+        })
+    }
     return (
-        <div className="relative z-0 flex h-full bg-windows-dark bg-cover bg-center text-white">
+        <div
+            className={`relative z-0 flex h-full ${
+                darkTheme ? "bg-windows-dark" : "bg-windows-light"
+            } bg-cover bg-center text-white`}
+        >
             <div className="left-0 m-10 flex w-full">
                 <DesktopIconsPanel></DesktopIconsPanel>
             </div>
             <div className="absolute bottom-0 left-0 flex w-full">
                 <TaskBar></TaskBar>
             </div>
+            <button onClick={changeTheme}>Change theme</button>
         </div>
     )
 }
