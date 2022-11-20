9be6dedb8eb74b2451b980cf07f6f228294fbd2f
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-11-20 02:04:03 -0600

    set localstorage for theme selection

--- src/context-api/Reducer.js
+++ src/context-api/Reducer.js
@@ -1,10 +1,14 @@
 export const initialState = {
-    darkTheme: true,
+    darkTheme: localStorage.getItem("darkTheme")
+        ? JSON.parse(localStorage.getItem("darkTheme"))
+        : true,
 }
 
 const reducer = (state, action) => {
     switch (action.type) {
         case "UPDATE_THEME":
+            // set the theme selection in local storage
+            localStorage.setItem("darkTheme", action.darkTheme)
             return {
                 ...state,
                 darkTheme: action.darkTheme,
