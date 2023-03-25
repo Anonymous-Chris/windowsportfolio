5a32a2a9e157b8c7e7c4e25500eee89a6655cab5
sk.niyara <sk.niyara@gmail.com>
2023-03-25 18:49:17 -0500

    add config for buttons

--- src/components/Pages/Projects/Buttons/GithubButton.js
+++ src/components/Pages/Projects/Buttons/GithubButton.js
@@ -1,8 +1,12 @@
 import React from "react"
 import { FiGithub } from "react-icons/fi"
-const GithubButton = () => {
+const GithubButton = ({ disabled }) => {
     return (
-        <div className="group mr-1 flex w-[inherit] max-w-[250px] items-center justify-center border-2 cursor-pointer">
+        <div
+            className={`group mr-1 flex w-[inherit] max-w-[250px] cursor-pointer items-center justify-center border-2 ${
+                disabled ? "cursor-not-allowed opacity-50" : ""
+            }`}
+        >
             <span className="mr-4">Github</span>
             <FiGithub className="absolute ml-[12rem] hidden group-hover:block"></FiGithub>
         </div>
--- src/components/Pages/Projects/ProjectDescription.js
+++ src/components/Pages/Projects/ProjectDescription.js
@@ -3,18 +3,17 @@ import GithubButton from "./Buttons/GithubButton"
 import LiveButton from "./Buttons/LiveButton"
 import HorizontalIconsStack from "./HorizontalIconsStack"
 import ProjectDescriptionDetail from "./ProjectDescriptionDetail"
-const ProjectDescription = ({ stackTextDesc, projectDescriptionDetail }) => {
+const ProjectDescription = ({ stackTextDesc, projectDesc, name, config }) => {
     return (
         <div className="flex w-full flex-col lg:w-[40%]">
+            {name.toUpperCase()}
             <div className="">
-                <ProjectDescriptionDetail
-                    projectDescriptionDetail={projectDescriptionDetail}
-                ></ProjectDescriptionDetail>
                 <HorizontalIconsStack stackTextDesc={stackTextDesc}></HorizontalIconsStack>
+                <ProjectDescriptionDetail projectDesc={projectDesc}></ProjectDescriptionDetail>
             </div>
             <div className="flex w-full justify-center">
-                <LiveButton></LiveButton>
-                <GithubButton></GithubButton>
+                <LiveButton disabled={!config.isLive}></LiveButton>
+                <GithubButton disabled={!config.hasGithub}></GithubButton>
             </div>
         </div>
     )
