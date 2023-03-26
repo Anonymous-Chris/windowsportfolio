aa83e7430870ca6ff2b3c7e52115cb78fd7e3c3c
sk.niyara <sk.niyara@gmail.com>
2023-03-25 19:32:57 -0500

    add exact true string

--- src/components/DesktopIconsPanel/DesktopIcons.js
+++ src/components/DesktopIconsPanel/DesktopIcons.js
@@ -6,8 +6,11 @@ const DesktopIcons = (props) => {
         props.clickedIcon(props.altText)
     }
     return (
-        <div className="flex h-20 w-20 cursor-pointer select-none  flex-col items-center justify-center rounded-md p-2 align-middle hover:bg-desktop-icons-hover">
-            <img draggable="false" className="h-11 w-11" src={props.source} alt={props.altText} onClick={clickedIcon} />
+        <div
+            onClick={clickedIcon}
+            className="flex h-20 w-20 cursor-pointer select-none  flex-col items-center justify-center rounded-md p-2 align-middle hover:bg-desktop-icons-hover"
+        >
+            <img draggable="false" className="h-11 w-11" src={props.source} alt={props.altText} />
             <span>{props.altText.charAt(0).toUpperCase() + props.altText.slice(1)}</span>
         </div>
     )
