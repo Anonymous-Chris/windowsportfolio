fa887a195c334fb58a5e85ab81c33981e596c0c6
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2023-03-31 15:21:40 -0500

    test

--- src/components/TaskBar/TaskBar.js
+++ src/components/TaskBar/TaskBar.js
@@ -14,63 +14,49 @@ import Weather from "./Weather"
 import Wifi from "./Wifi"
 const TaskBar = () => {
     const [
-        {
-            windowsIconClicked,
-            chromeIconClicked,
-            microsoftedgeClicked,
-            fileIconClicked,
-            upArrowIconClicked,
-        },
-        dispatch,
+        { windowsIconClicked, chromeIconClicked, microsoftedgeClicked, fileIconClicked, upArrowIconClicked },
+        dispatch
     ] = useStateValue()
 
     const clickedIcon = (clicked) => {
         dispatch({
             type: "UPDATE_TASKBAR_ACTION",
-            windowsIconClicked:
-                clicked === "windowsIcon"
-                    ? !windowsIconClicked
-                    : windowsIconClicked,
-            chromeIconClicked:
-                clicked === "chromeIcon"
-                    ? !chromeIconClicked
-                    : chromeIconClicked,
-            microsoftedgeClicked:
-                clicked === "microsoftedge"
-                    ? !microsoftedgeClicked
-                    : microsoftedgeClicked,
-            fileIconClicked:
-                clicked === "fileIcon" ? !fileIconClicked : fileIconClicked,
-            upArrowIconClicked:
-                clicked === "upArrowIcon"
-                    ? !upArrowIconClicked
-                    : upArrowIconClicked,
+            windowsIconClicked: clicked === "windowsIcon" ? !windowsIconClicked : windowsIconClicked,
+            chromeIconClicked: clicked === "chromeIcon" ? !chromeIconClicked : chromeIconClicked,
+            microsoftedgeClicked: clicked === "microsoftedge" ? !microsoftedgeClicked : microsoftedgeClicked,
+            fileIconClicked: clicked === "fileIcon" ? !fileIconClicked : fileIconClicked,
+            upArrowIconClicked: clicked === "upArrowIcon" ? !upArrowIconClicked : upArrowIconClicked
         })
     }
     return (
         <div className="flex w-full select-none">
             <div className="flex w-[70%] items-center justify-center gap-3 bg-taskbar pl-[30%] text-center align-middle">
                 <TaskBarIcons
+                    disabled={true}
                     source={WindowsIcon}
                     altText="windowsIcon"
                     clickedIcon={clickedIcon}
                 ></TaskBarIcons>
                 <TaskBarIcons
+                    disabled={false}
                     source={ChromeIcon}
                     altText="chromeIcon"
                     clickedIcon={clickedIcon}
                 ></TaskBarIcons>
                 <TaskBarIcons
+                    disabled={false}
                     source={MicrosoftEdge}
                     altText="microsoftedge"
                     clickedIcon={clickedIcon}
                 ></TaskBarIcons>
                 <TaskBarIcons
+                    disabled={true}
                     source={FileIcon}
                     altText="fileIcon"
                     clickedIcon={clickedIcon}
                 ></TaskBarIcons>
                 <TaskBarIcons
+                    disabled={false}
                     source={GithubIcon}
                     altText="githubIcon"
                     clickedIcon={clickedIcon}
@@ -81,11 +67,7 @@ const TaskBar = () => {
                 className="z-10 flex w-[30%] items-center justify-end gap-2 bg-taskbar
                 text-center align-middle text-xl"
             >
-                <UpArrow
-                    altText="upArrowIcon"
-                    clickedIcon={clickedIcon}
-                    show={upArrowIconClicked}
-                ></UpArrow>
+                <UpArrow altText="upArrowIcon" clickedIcon={clickedIcon} show={upArrowIconClicked}></UpArrow>
                 <Weather></Weather>
                 <Wifi></Wifi>
                 <Battery></Battery>
