26afd6641cf8dcff21f53dc938631ade609ddd52
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2023-06-22 09:32:54 -0500

    refactor: add skill assets

--- src/components/Pages/Projects/HorizontalIconsStack.js
+++ src/components/Pages/Projects/HorizontalIconsStack.js
@@ -1,16 +1,23 @@
 import React from "react"
-import chartJsLogo from "../../../assets/skills/chartjs-original.svg"
+import ChartJsLogo from "../../../assets/skills/chartjs-original.svg"
+import CSharpLogo from "../../../assets/skills/csharp-original.svg"
 import CssLogo from "../../../assets/skills/css3-original.svg"
+import DotNetLogo from "../../../assets/skills/dotnet-original.svg"
+import EChartsLogo from "../../../assets/skills/echarts-original.svg"
 import ExpressLogo from "../../../assets/skills/express-original.svg"
 import HtmlLogo from "../../../assets/skills/html5-original.svg"
+import JavaLogo from "../../../assets/skills/java-original.svg"
 import JavascriptLogo from "../../../assets/skills/javascript-original.svg"
 import LinuxLogo from "../../../assets/skills/linux-original.svg"
 import MongodbLogo from "../../../assets/skills/mongodb-original.svg"
-import mysqlLogo from "../../../assets/skills/mysql-original.svg"
-import nextJSLogo from "../../../assets/skills/nextjs-original.svg"
+import MysqlLogo from "../../../assets/skills/mysql-original.svg"
+import NextJSLogo from "../../../assets/skills/nextjs-original.svg"
+import PhotopeaLogo from "../../../assets/skills/photopea-original.svg"
 import ReactLogo from "../../../assets/skills/react-original.svg"
 import ReduxLogo from "../../../assets/skills/redux-original.svg"
+import TailwindLogo from "../../../assets/skills/tailwindcss-original.svg"
 import VueLogo from "../../../assets/skills/vue-original.svg"
+
 import Logo from "../About/Logo"
 
 const HorizontalIconsStack = ({ stackTextDesc }) => {
@@ -36,11 +43,23 @@ const HorizontalIconsStack = ({ stackTextDesc }) => {
         } else if (name === "mongodb") {
             return <Logo name="Mongodb" logo={MongodbLogo} hover={"true"}></Logo>
         } else if (name === "mysql") {
-            return <Logo name="MySQL" logo={mysqlLogo} hover={"true"}></Logo>
+            return <Logo name="MySQL" logo={MysqlLogo} hover={"true"}></Logo>
         } else if (name === "next") {
-            return <Logo name="Next JS" logo={nextJSLogo} hover={"true"}></Logo>
+            return <Logo name="Next JS" logo={NextJSLogo} hover={"true"}></Logo>
         } else if (name.includes("chart")) {
-            return <Logo name="Chart JS" logo={chartJsLogo} hover={"true"}></Logo>
+            return <Logo name="Chart JS" logo={ChartJsLogo} hover={"true"}></Logo>
+        } else if (name.includes("csharp")) {
+            return <Logo name="C#" logo={CSharpLogo} hover={"true"}></Logo>
+        } else if (name.includes("dotnet")) {
+            return <Logo name=".NET" logo={DotNetLogo} hover={"true"}></Logo>
+        } else if (name.includes("echarts")) {
+            return <Logo name="ECharts" logo={EChartsLogo} hover={"true"}></Logo>
+        } else if (name.includes("java")) {
+            return <Logo name="Java" logo={JavaLogo} hover={"true"}></Logo>
+        } else if (name.includes("photopea")) {
+            return <Logo name="Photopea" logo={PhotopeaLogo} hover={"true"}></Logo>
+        } else if (name.includes("tailwind")) {
+            return <Logo name="Tailwind" logo={TailwindLogo} hover={"true"}></Logo>
         }
     }
     return (
